1addebf0540b00d45cd2d65759cc36cb69479961
Colin Osborn <colinosborn@Tech-MacBook-Pro.local>
2017-10-31 11:17:03 -0600

    gaining speed and back to it

--- exercises/all_pattern_test.rb
+++ exercises/all_pattern_test.rb
@@ -51,44 +51,57 @@ class AllPatternTest < Minitest::Test
   end
 
   def test_not_all_empty
-    skip
     strings = ["", "", "", "full", "", "", ""]
-    # Your code goes here
+    all_empty = true
+      strings.each do |string|
+        all_empty = false if string != ""
+      end
     refute all_empty
   end
 
   def test_not_all_uppercase
-    skip
     words = ["DOUGHNUT", "CASH", "MAIN", "bOWl", "SMACK", "SAND"]
-    # Your code goes here
+    all_caps = true
+      words.each do |word|
+        all_caps = false if word.upcase
+      end
     refute all_caps
   end
 
   def test_all_lies
-    skip
     lies = [false, false, false, false]
-    # Your code goes here
+    all_lies = true
+      lies.each do |lie|
+        all_lies = false if lies == true
+      end
     assert all_lies
   end
 
   def test_all_multiples_of_7
-    skip
     numbers = [42, 14, 35, 49, 28, 56, 21, 7]
-    # Your code goes here
+    all_multiples_of_7 = true
+      numbers.each do |number|
+        all_multiples_of_7 = true if number % 7 == 0
+      end
     assert all_multiples_of_7
   end
 
   def test_not_all_3_digits_long
     skip
     numbers = [981, 831, 509, 332, 892, 8999, 110]
-    # Your code goes here
+    all_3_digits = true
+    numbers.each do |number|
+      all_3_digits = true unless number.length != 3
+    end
     refute all_3_digits
   end
 
   def test_all_4_letter_words
-    skip
     words = ["love", "hate", "fire", "bird", "call"]
-    # Your code goes here
+    all_4_letters = true
+    words.each do |word|
+      all_4_letters = true unless word.length != 4
+    end
     assert all_4_letters
   end
 
